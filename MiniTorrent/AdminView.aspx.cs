9a611d1224bd5062901ae2b513560e53ba6fe764
vigalva <igalsoviet15@gmail.com>
2019-02-12 19:39:56 +0000

    igal's version

--- MiniTorrent/AdminView.aspx.cs
+++ MiniTorrent/AdminView.aspx.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MiniTorrent.Controllers;
+using MiniTorrent.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +11,14 @@ namespace MiniTorrent
 {
     public partial class AdminView : System.Web.UI.Page
     {
+        AdminsController adminController;
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            adminController = new AdminsController();
+            List<MyFile> files = adminController.ShowAllFiles();
+            lblAllFiles.Text = files.Count().ToString();
+            List<User> users = adminController.ShowAllActiveUsers();
+            lblActiveUsers.Text = users.Count().ToString();
         }
 
        
